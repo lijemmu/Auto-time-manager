500f1574d1da95913265af3299214e591d429a59
Joshua Feliciano <joshfeli613@gmail.com>
2022-07-20 17:49:14 +0000

    Created outline of JavaScript file that will serve as the control center

---
+++ frontend/api.js
@@ -0,0 +1,16 @@
+// first step: grab calendar data from API (work with Fernanda to see how this is done)
+
+
+// function: format data from user's calendar into JSON to send to Flask app
+
+
+// function (triggered by button): send the data to Flask app
+
+
+// get the response from the Flask app, which should hopefully be JSON formatted in the same way
+
+
+// post the data to the calendar API to update the user's calendar
+
+
+// maybe alert the user on the frontend that the task was successful (or if there was an error)
