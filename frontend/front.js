3079fb25262cc79d73ad6b4685791da86604cc82
Fernanda Pisconte <fpisconte@berkeley.edu>
2022-07-21 14:13:23 -0500

    Storing new divs

--- frontend/front.js
+++ frontend/front.js
@@ -14,13 +14,16 @@ document
 
 var task = document.getElementById("make-task");
 
-document
-  .querySelector("#make-task")
-  .addEventListener("submit", function (event) {
-    var activity = task.activity.value;
-    var duration = task.duration.value;
-    var preference = task.preference.value;
+var tasksList = [];
 
+function loadTasks() {
+  if (!localStorage.tasksList) {
+    return false;
+  }
+
+  var currentTasksList = JSON.parse(localStorage.tasksList);
+
+  for (i = 0; i < currentTasksList.length; i++) {
     //makes a new div with the content
     var tasks = document.getElementById("task-items-container");
     var new_div = document.createElement("div");
@@ -28,23 +31,43 @@ document
     new_div.class = "one-task";
     new_div.innerHTML =
       '<div class="one-task"><div class="upper"> <p class="task-activity">' +
-      activity +
+      currentTasksList[i].activity +
       '</p> <div class="duration-clock"> <p class="task-duration">' +
-      duration +
+      currentTasksList[i].duration +
       "h" +
       '</p> <img class="small-icon" src="images/clock.png" /></div></div><div class="lower"><p class="task-time">' +
       "July 5th, " +
-      preference +
+      currentTasksList[i].preference +
       '</p><img class="small-icon" src="images/pencil.png" /></div></div>';
 
-    //hides the form
-    task.style.display = "none";
+    if (tasks.childElementCount > currentTasksList.length) {
+      for (n = 0; tasks.childElementCount - currentTasksList.length; n++) {
+        tasks.removeChild(tasks.firstChild);
+      }
+    }
+  }
+}
 
+document
+  .querySelector("#make-task")
+  .addEventListener("submit", function (event) {
+    var oneTask = {
+      activity: document.getElementById("activity").value,
+      duration: document.getElementById("duration").value,
+      preference: document.getElementById("preference").value,
+      date: "July 5th",
+    };
+
+    tasksList[tasksList.length] = oneTask;
+    localStorage.tasksList = JSON.stringify(tasksList);
+    loadTasks();
+    task.style.display = "none";
     event.preventDefault();
+    return false;
   });
 
 document.querySelector("#new-task").addEventListener("click", function () {
-  //display form
-
   task.style.display = "block";
 });
+
+loadTasks();
