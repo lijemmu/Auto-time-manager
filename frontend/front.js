daae5f90f59eac96320c01edb162c8d67db3decf
Joshua Feliciano <41080599+joshfeli@users.noreply.github.com>
2022-07-21 13:29:36 -0400

    Testing extension works in browser
    Co-authored-by: Emmanuel Kebede <emmanuelermias@gmail.com>
    Fernanda Pisconte <fpisconte@berkeley.edu>

--- frontend/front.js
+++ frontend/front.js
@@ -1,6 +1,7 @@
 var start_end = document.getElementById("start-end");
 
-document.querySelector("#start-end").addEventListener("submit", function () {
+document.querySelector("#start-end").addEventListener("submit", function (e) {
+  e.preventDefault();
   var start = start_end.start.value;
   var end = start_end.end.value;
   console.log("123456");
@@ -8,6 +9,7 @@ document.querySelector("#start-end").addEventListener("submit", function () {
 
 var task = document.getElementById("make-task");
 
-document.querySelector("#new-task").addEventListener("click", function () {
+document.querySelector("#new-task").addEventListener("click", function (e) {
+  e.preventDefault();
   task.style.display = "block";
 });
