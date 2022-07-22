167d92344b97dd1b8d045e13b684164d54367821
Joshua Feliciano <41080599+joshfeli@users.noreply.github.com>
2022-07-21 21:13:23 -0400

    Added subtractFromTimeAvailable function into front.js

--- frontend/front.js
+++ frontend/front.js
@@ -76,6 +76,14 @@ document
     return false;
   });
 
+var timeAvailable = document.getElementById("num-hours-left")
+function subtractFromTimeAvailable(taskDuration){
+  currentTimeLeft = parseFloat(timeAvailable.innerText)
+  currentTimeLeft -= taskDuration
+  currentTimeLeft = Math.round(currentTimeLeft * 10) / 10
+  timeAvailable.innerText = currentTimeLeft
+}
+
 document.querySelector("#new-task").addEventListener("click", function () {
   loadTasks();
   task.style.display = "block";
